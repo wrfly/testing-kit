a4055cb6640b3b213961f4eafd212bb012a058fa
mrfly <mr.wrfly@gmail.com>
2018-02-06 13:18:12 +0800

    set total number (ignore rate)

--- stress-testing/creazy-udp/main.go
+++ stress-testing/creazy-udp/main.go
@@ -13,11 +13,12 @@ import (
 )
 
 var (
-	remote    *string
+	target    *string
 	data      *string
 	rate      *int
 	port      *int
 	clientNum *int
+	ttnum     *uint64
 
 	connGroup []net.Conn
 
@@ -27,8 +28,9 @@ var (
 func main() {
 	cpuNum := runtime.NumCPU()
 
-	rate = flag.Int("n", 10000, "send rate, <num>/s, set to -1 for ultimate")
-	remote = flag.String("r", "localhost:12345", "remote addr to send packet")
+	rate = flag.Int("r", 10000, "send rate, <num>/s, set to -1 for ultimate")
+	ttnum = flag.Uint64("n", 0, "total number, set to positive for counter mode (ignore rate)")
+	target = flag.String("t", "localhost:12345", "target addr to send packet")
 	data = flag.String("d", "", "data to send, empty by default")
 	clientNum = flag.Int("c", cpuNum, "client num, default is `runtime.NumCPU()`")
 	flag.Parse()
@@ -36,11 +38,8 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	log.Printf("send '%s' to [%s] with [%d] clients at %d/s", *data,
-		*remote, *clientNum, *rate)
-
 	for i := 0; i < *clientNum; i++ {
-		conn, err := net.Dial("udp", *remote)
+		conn, err := net.Dial("udp", *target)
 		if err != nil {
 			log.Printf("conn error: %s", err)
 			return
@@ -48,7 +47,9 @@ func main() {
 		connGroup = append(connGroup, conn)
 	}
 
-	if *rate > 0 {
+	if *rate > 0 && *ttnum == 0 {
+		log.Printf("send '%s' to [%s] with [%d] clients at %d/s\n", *data,
+			*target, *clientNum, *rate)
 		go func() {
 			numPerClient := *rate / *clientNum
 			for {
@@ -63,12 +64,29 @@ func main() {
 				time.Sleep(time.Second)
 			}
 		}()
-	} else {
+	} else if *rate < 0 && *ttnum == 0 {
+		log.Printf("send '%s' to [%s] with [%d] clients in ultimate\n", *data,
+			*target, *clientNum)
 		go func() {
 			for {
 				for _, conn := range connGroup {
 					atomic.AddUint64(&sentPkt, 1)
 					conn.Write([]byte(*data))
+				}
+			}
+		}()
+	} else {
+		log.Printf("send '%s' to [%s] with [%d] clients fot [%d] times\n", *data,
+			*target, *clientNum, *ttnum)
+		go func() {
+			defer log.Printf("Send %d packets, done\n", *ttnum)
+			for {
+				for _, conn := range connGroup {
+					atomic.AddUint64(&sentPkt, 1)
+					conn.Write([]byte(*data))
+					if atomic.LoadUint64(&sentPkt) >= *ttnum {
+						return
+					}
 				}
 			}
 		}()
@@ -98,10 +116,9 @@ func statistic(ctx context.Context) {
 		case <-ctx.Done():
 			return
 		case <-tk.C:
-			s = atomic.LoadUint64(&sentPkt)
-			atomic.StoreUint64(&sentPkt, 0)
-			if s != 0 {
-				log.Printf("send: %v/s\n", s)
+			if n := atomic.LoadUint64(&sentPkt) - s; n != 0 {
+				s = atomic.LoadUint64(&sentPkt)
+				log.Printf("Send: %v/s\tTotal sent %v\n", n, s)
 			}
 		}
 	}
